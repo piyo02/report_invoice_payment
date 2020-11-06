d19de1b93b19ea1373738c3cb0131927afdf686e
piyo02 <alzidni2000@gmail.com>
2020-11-06 08:21:12 +0800

    push

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,16 @@
 {
-    'name': 'Template Addon',
+    'name': 'Report Invoice Payment',
     'author': 'technoindo.com',
     'category': 'hidden',
     'version': '10.0',
     'summary': 'Summary the addon.',
     'description': '''Description the addon'''
                    ,
-    'depends': ['base'],
+    'depends': ['account'],
     'data': [
-        ''
+        'reports/invoice_payment_report.xml',
+        'reports/invoice_payment_temp.xml',
+        'wizard/report_invoice_payment_wizard.xml',
     ],
     'images': [''],
     'auto_install': False,
